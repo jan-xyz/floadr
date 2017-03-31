e146ee66c1de2246de263ba3f13b1232f9308b09
Jan Steinke <jan.steinke@gmail.com>
2017-03-31 22:35:24 +0200

    let floadr handle the setup

--- lib/floadr.rb
+++ lib/floadr.rb
@@ -1,5 +1,15 @@
 class Floadr
+  def initialize
+    @flickr = Flickr.new
+    @flickr.initialize_api_key()
+    @downloadr = Downloadr.new
+  end
 
+  def process(input)
+    photo_id, _ = @flickr.get_most_interesting_photo(input)
+    photo_url = @flickr.get_photo_url(photo_id)
+    @downloadr.download_image(photo_url)
+  end
 end
 
 require 'floadr/flickr'
