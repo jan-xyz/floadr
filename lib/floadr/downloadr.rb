d7c6f396d83a6ec47dc86c6a262f988953096d3b
Jan Steinke <jan.steinke@gmail.com>
2017-03-31 22:34:43 +0200

    fix crash when cache_dir already exists

--- lib/floadr/downloadr.rb
+++ lib/floadr/downloadr.rb
@@ -3,9 +3,10 @@ require 'digest/md5'
 class Downloadr
 
   def download_image(url)
-    Dir.mkdir './downloads'
+    cache_dir = './downloads'
+    Dir.mkdir cache_dir unless File.exists?(cache_dir)
     filename = Digest::MD5.hexdigest(url)
-    full_file_path = "./downloads/%s.jpg" % filename
+    full_file_path = "%s/%s.jpg" % [cache_dir, filename]
     open(url) {|f|
        File.open(full_file_path,"wb") do |file|
          file.puts f.read
