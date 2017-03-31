6b36b6462f541ccafd4b7282101b46c64bbf8a8c
Jan Steinke <jan.steinke@gmail.com>
2017-03-31 20:31:14 +0200

    add functions to set the Flickr API key

--- lib/floadr/flickr.rb
+++ lib/floadr/flickr.rb
@@ -1,8 +1,20 @@
+require 'flickraw'
+
 class Flickr
   def initialize()
+    FlickRaw.api_key=""
+    FlickRaw.shared_secret=""
   end
 
-  def querry(querry_list)
+  def set_api_key(api_key)
+    FlickRaw.api_key=api_key
+  end
+
+  def get_api_key()
+    return FlickRaw.api_key
+  end
+
+  def querry(search_text)
   end
 
   def is_querry_list_long_enough(querry_list)
--- test/test_flickr.rb
+++ test/test_flickr.rb
@@ -19,4 +19,11 @@ class FlickrTest < Minitest::Test
     test_querry_list = Array.new(11, "test")
     assert_equal true, sut.is_querry_list_long_enough(test_querry_list)
   end
+
+  def test_setting_flickr_api_key
+    sut = Flickr.new
+    test_api_key = "test_this"
+    sut.set_api_key(test_api_key)
+    assert_equal test_api_key, sut.get_api_key()
+  end
 end
