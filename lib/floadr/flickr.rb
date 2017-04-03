f0215126447b2fc98e08b57d388e35ff594b7ea4
Jan Steinke <jan.steinke@gmail.com>
2017-04-03 19:51:43 +0200

    remove unused function to check length of list

--- lib/floadr/flickr.rb
+++ lib/floadr/flickr.rb
@@ -60,13 +60,4 @@ class Flickr
     photo_url = FlickRaw.url_b(info)
     return photo_url
   end
-
-  def is_querry_list_long_enough(querry_list)
-    list_length = querry_list.length()
-    if list_length >= 10
-      return true
-    else
-      return false
-    end
-  end
 end
--- test/test_flickr.rb
+++ test/test_flickr.rb
@@ -7,21 +7,6 @@ class FlickrTest < Minitest::Test
     @sut = Flickr.new
   end
 
-  def test_querry_length_with_10_items
-    test_querry_list = Array.new(10, "test")
-    assert_equal true, @sut.is_querry_list_long_enough(test_querry_list)
-  end
-
-  def test_querry_length_with_1_items
-    test_querry_list = Array.new(1, "test")
-    assert_equal false, @sut.is_querry_list_long_enough(test_querry_list)
-  end
-
-  def test_querry_length_with_11_items
-    test_querry_list = Array.new(11, "test")
-    assert_equal true, @sut.is_querry_list_long_enough(test_querry_list)
-  end
-
   def test_setting_flickr_api_key
     test_api_key = "test_this"
     @sut.set_api_key(test_api_key)
